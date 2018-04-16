f724ca8a4c09ad88b67cf03f8fa3bc48af7dfff6
Fuxi <327714392@qq.com>
2018-04-16 20:24:11 +0800

    Signed-off-by: Fuxi <327714392@qq.com> 制造冲突

--- thinkphp5/application/admin/controller/Index.php
+++ thinkphp5/application/admin/controller/Index.php
@@ -5,11 +5,8 @@ class Index
 {
     public function index()
     {
-<<<<<<< HEAD
 
-            return view("/index");
-=======
-        return $this->fetch('index');
->>>>>>> 160d7f280d19e35f8bb36573d3c49a41e1ca00b9
+        return view("/index");
+
     }
 }
