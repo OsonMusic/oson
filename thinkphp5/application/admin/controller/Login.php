4baa522bfb8314b0322ba7101ca9d4392d26a6fe
Fuxi <327714392@qq.com>
2018-04-17 14:54:20 +0800

    Signed-off-by: Fuxi <327714392@qq.com> 修改

---
+++ thinkphp5/application/admin/controller/Login.php
@@ -0,0 +1,27 @@
+<?php
+namespace app\admin\controller;
+use app\admin\model\User;
+
+class Login  extends \think\Controller
+{
+    public function index(){
+        return $this->fetch("login/login");
+    }
+    public function loginDO(){
+        $name = input('post.name');
+//        print_r($name);die;
+        $pwd = input('post.pwd');
+        $user = new User();
+        $res = $user->getLogin($name);
+
+
+
+
+
+
+
+    }
+
+
+
+}
---
+++ thinkphp5/application/admin/model/User.php
@@ -0,0 +1,11 @@
+<?php
+namespace app\admin\model;
+use think\Model;
+use think\Db;
+class User extends Model{
+    public $tableName = "oson_user";
+    public function getLogin($str=''){
+        $res =   Db::table($this->tableName)->where('user_name',$str)->find();
+        return $res;
+    }
+}
