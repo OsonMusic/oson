df3a1f93a03039f34f8e3f88e48209f8c6871abc
liuke1680 <1953524721@qq.com>
2018-04-23 13:52:01 +0800

    Signed-off-by: liuke1680 <1953524721@qq.com> 评论详情

--- thinkphp5/application/admin/model/Advertmodel.php
+++ thinkphp5/application/admin/model/Advertmodel.php
@@ -5,13 +5,34 @@ use think\Db;
 class Advertmodel extends Model
 {
     public $tablename = "oson_advert";
+
     public function insertData($data)
     {
         return Db::table($this->table)->insertGetId($data);
     }
+
     public function select($data = null)
     {
         $res = db($data)->select();
         return $res;
     }
+
+    public function hotshow($table, $str)
+    {
+        $res = Db::table($table)->where('hot', $str)->select();
+        return $res;
+    }
+
+    public function sql($sql)
+    {
+        $res = Db::query($sql);
+        return $res;
+    }
+
+    public function com($table,$key,$str)
+    {
+        $res = Db::table($table)->where($key,$str)->select();
+        return $res;
+    }
+
 }
